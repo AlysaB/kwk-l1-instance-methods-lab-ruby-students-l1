bbb35c697fbd49d6b67faddd5859cd7051537323
Alysa Barbarisi <baal02@cox.net>
2018-06-07 21:28:17 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # person.rb
+
+class Person 
+  
+  def talk
+    puts "Hello World!"
+  end
+  
+  def walk
+    puts "The Person is walking"
+  end
+end 
+
+ human =  Person.new 
+ puts human.talk 
+ puts human.walk 
